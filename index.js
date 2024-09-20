755ad5f3359c21fa58f46e5247f530c216c18630
Ariana <152944814+aam214@users.noreply.github.com>
2024-09-20 15:04:46 -0500

    Changed variable name at top of JS file to starsRating. Added .forEach method with a click event listener. Created function of showResult with .forEach method and if/else statement for adding and removing active class when stars are clicked.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const stars = document.querySelectorAll(".fa-star");
-console.log(stars);
+const starsRating = document.querySelectorAll(".fa-star");
+
+
+starsRating.forEach((starRating, index) => {
+starRating.addEventListener("click", () => {
+console.log("clicked", index);
+
+showResult(index);
+  });
+});
+
+function showResult(index) {
+  starsRating.forEach((starRating, idx)=> {
+    if (idx < index +1) {
+      //console.log(idx);
+      starRating.classList.add("active");
+    }else{
+      starRating.classList.remove("active");
+    }
+  });
+}
+
