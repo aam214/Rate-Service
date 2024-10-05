6a7ca0beae7ff7c76d06d529707d9351ed283a33
Ariana <152944814+aam214@users.noreply.github.com>
2024-10-05 12:47:05 -0500

    removed console.log

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const starsRating = document.querySelectorAll(".fa-star");
 const emojisFace = document.querySelectorAll(".fa-regular");
+
 const changeColors = ["red", "orange", "yellow", "lightseagreen", "green"];
 
+
+//index is the star being clicked by the user
 starsRating.forEach((starRating, index) => {
 starRating.addEventListener("click", () => {
 showResult(index);
   });
 });
 
+
+//idx is the star that was clicked previously
 function showResult(index) {
   starsRating.forEach((starRating, idx)=> {
+     //if your current star is less than the star now being clicked
     if (idx < index +1) {
-      //console.log(idx);
+    
       starRating.classList.add("active");
     }else{
       starRating.classList.remove("active");
